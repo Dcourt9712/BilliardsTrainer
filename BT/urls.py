0fae987d7910306a177826165e1dc2f5b895ca03
collinsjacob127 <collinsjacob127@gmail.com>
2023-09-27 11:26:45 -0700

    testing views

--- BT/urls.py
+++ BT/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from main_app import views as MA_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', MA_views.home, name = 'home'),
+    path("main_app/", include("main_app.urls")),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -5,4 +5,25 @@ from django.http import HttpResponse
 def home(request):
     return render(request,'main_app/home.html')
 
+def index(request):
+    return render(request, 'main_app/form.html')
 
+def search(request):
+    return render(request, 'main_app/form.html')
+
+def submit(request):
+    search_id = request.POST.get('textfield', None)
+    print(search_id)
+    if request.method == 'POST':
+        search_id = request.POST.get('textfield', None)
+        try:
+            user = search_id
+            #do something with user
+            html = (f'<H1>{user}</H1>')
+            return HttpResponse(html)
+        except (search_id == None or search_id == ''):
+            print("bad response")
+            response = ("<H1>Bad Input</H1> <H2><a href='main_app/form.html'>Go back</a><H2>")
+            return HttpResponse(response)  
+    else:
+        return render(request, 'main_app/form.html')
